30aa59f48c0a7132c8fa18c4070146722f2b1ca7
Juan Torres <torrescjuan@javeriana.edu.co>
2025-09-07 20:48:23 -0500

    Add layout and navbar components; integrate monitoring page with heatmap functionality

--- src/pages/Monitoring/Monitoring.tsx
+++ src/pages/Monitoring/Monitoring.tsx
@@ -1,9 +1,42 @@
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet.heat";
+import { useEffect } from "react";
+
+const HeatmapLayer = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    const heatData: [number, number, number?][] = [
+      [4.65, -74.1, 0.8],
+      [4.61, -74.08, 0.6],
+      [4.63, -74.07, 0.9],
+      [4.60, -74.05, 0.4],
+    ];
+
+    // @ts-ignore
+    L.heatLayer(heatData, { radius: 25 }).addTo(map);
+  }, [map]);
+
+  return null;
+};
+
 const Monitoring = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-white">
-      <h1 className="text-3xl font-bold text-green-600">
-        Página de Monitoreo
-      </h1>
+    <div className="h-screen w-full">
+      <MapContainer
+        center={[4.6097, -74.0817]} // Bogotá
+        zoom={12}
+        scrollWheelZoom={true}
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <HeatmapLayer />
+      </MapContainer>
     </div>
   );
 };
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Monitoring from "../pages/Monitoring/Monitoring";
-
+import Layout from "../components/layout/Layout";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home sin navbar */}
         <Route path="/" element={<Home />} />
-        <Route path="/monitoring" element={<Monitoring />} />
+
+        {/* Pantallas con navbar */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Monitoring />
+            </Layout>
+          }
+        />
+        <Route
+          path="/monitoring"
+          element={
+            <Layout>
+              <Monitoring />
+            </Layout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
